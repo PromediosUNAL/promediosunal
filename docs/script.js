0891d2e853d6b95d454a7cb2f6a2c7a2efe4859f
Camilo Alejandro <camiloalejandrolagosmalaver@gmail.com>
2024-08-24 22:29:14 -0500

    Mejoras de ui y modo oscuro (hay bugs en celulares)

--- docs/script.js
+++ docs/script.js
@@ -265,3 +265,9 @@ function toggleTablaYBoton() {
         botonAgregarFila.classList.add('disabled');
     }
 }
+
+const toggle = document.getElementById('dark-mode-toggle');
+toggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+});
+
